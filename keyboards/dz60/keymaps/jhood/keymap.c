88bf7ea2e0e5472b1f1c472f9dffbf5423a51c35
Jonathan Hood <jhood06@gmail.com>
2021-01-29 15:18:43 -0600

    adjust dz60 keymap to avoid reseting my keyboard - make ` work better.

--- keyboards/dz60/keymaps/jhood/keymap.c
+++ keyboards/dz60/keymaps/jhood/keymap.c
@@ -10,10 +10,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11    ,  KC_F12 , _______, KC_DEL,
-        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______   , KC_PGUP , _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDOWN ,           KC_HOME,
-        _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, KC_UP     , KC_END  , _______,
-        RESET  , _______, _______,                   _______, _______, _______,          KC_LEFT, KC_DOWN, _______   , KC_RIGHT, _______
+        KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11    , KC_F12  , _______, KC_DEL,
+        RESET  , RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______   , KC_PGUP , KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDOWN ,           KC_END,
+        _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, KC_UP     , _______ , _______,
+        _______, _______, _______,                   _______, _______, _______,          KC_LEFT, KC_DOWN, _______   , KC_RIGHT, _______
     )
 };
